2c7e286b5e9e7064820a867332e0dc4e613954db
BadriNarayananN <143417569+BadriNarayananN@users.noreply.github.com>
2023-09-11 16:56:45 +0530

    Add files via upload

---
+++ Lab2/PGM9.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter three points: ");
+	scanf("%d %d %d",&a,&b,&c);
+	if (a>b && a>c)
+		printf("%d is great",a);
+	else if (b>c)
+		printf("%d is great",b);
+	else
+		printf("%d is great",c);
+	return 0;
+}
